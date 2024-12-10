cd897b3b055966323020b5df872f7df822d30d7f
JoeyEmeryWubbleyou <167451463+JoeyEmeryWubbleyou@users.noreply.github.com>
2024-12-10 11:15:57 +0000

    LazyImage

---
+++ src/components/LazyImage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+        class="w-full relative min-h-24"
+        :class="`lazy-image-${id}`"
+    >
+        <img
+            v-if="loadedImage"
+            :src="loadedImage"
+            :class="imageClasses"
+            class="relative z-10"
+        />
+
+        <slot />
+    </div>
+</template>
+
+<script>
+    import { useId } from 'vue'
+
+    export default {
+        props: {
+            image: {
+                required: true,
+            },
+            imageClasses: {
+                default: '',
+            },
+        },
+
+        computed: {
+            id()
+            {
+                return useId()
+            }
+        },
+
+        data() {
+            return {
+                loadedImage: null,
+            }
+        },
+
+        mounted()
+        {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        this.loadedImage = this.image
+                        observer.unobserve(entry.target)
+                    }
+                })
+            })
+
+            observer.observe(document.querySelector(`.lazy-image-${this.id}`))
+        }
+    }
+</script>
